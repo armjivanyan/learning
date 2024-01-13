2b52b503c29bcacdc57ce0d46f7b9075860f1042
armjivanyan <arm.jivanyan@gmail.com>
2024-01-13 13:35:53 +0400

    added 4 console logs

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello world 1");
+console.log("Hello world 2");
+console.log("Hello world 3");
+console.log("Hello world 4");
+
+//create new branch -   git checkout -b branchName
+// git checkout branchName
+// git pull
+// git add .
+// git commit -m "added 4 console logs"
+// git push
+//
+//
+//
